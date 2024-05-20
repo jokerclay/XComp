fc030a65f9c1ef236959e0bc6c63fa66efdbbe0e
jokerclay <2079209547@qq.com>
2024-05-20 17:00:00 +0800

    添加项目文件。

---
+++ XComponents/XButton/XButton.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace XComp.XComponents.XButton;
+
+public partial class XButton
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public bool Disabled { get; set; }
+    
+}
